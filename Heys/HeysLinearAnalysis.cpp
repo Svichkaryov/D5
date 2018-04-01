827d0f9361c5b65dd8b354a4f81e136dd0202a18
Ivan Svichkaryov <sv.ivan1996@gmail.com>
2018-04-01 23:35:50 +0300

    rm HLA.cpp|h

--- Heys/HeysLinearAnalysis.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "HeysLinearAnalysis.h"
-
-
-
-std::map<int, double> HeysLinearAnalysis::differentialSearch(int a, int sBoxNumber)
-{
-	return std::map<int, double>();
-}
